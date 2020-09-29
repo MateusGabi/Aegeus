de329fdd6a725d62d08b067965fda574dcff43f5
Mateus <mateusgabimoreira@gmail.com>
2020-09-29 20:12:29 -0300

    refactor(IServiceDescriptorBuilder): added throwing error

--- src/main/java/br/unicamp/ic/laser/model/IServiceDescriptorBuilder.java
+++ src/main/java/br/unicamp/ic/laser/model/IServiceDescriptorBuilder.java
@@ -2,6 +2,8 @@ package br.unicamp.ic.laser.model;
 
 import br.unicamp.ic.laser.readers.IInputFile;
 
+import java.io.IOException;
+
 public interface IServiceDescriptorBuilder extends IInputFile {
-    IServiceDescriptor build(String filePath);
+    IServiceDescriptor build(String filePath) throws IOException;
 }
